b7cbce31e4f31ecf65302259a708d1e86fd22979
OmegaChronos <davide.cioni.128@gmail.com>
2021-08-18 17:23:49 +0200

    Definition of constants for number of objects in level + declaration of first fields in class

--- level.hpp
+++ level.hpp
@@ -1,8 +1,16 @@
 #ifndef LEVEL
 #define LEVEL
+#include "player.hpp"
+#define N_ENEMIES: 4
+#define N_BONUS: 2
+#define N_PLATFORMS: 6 
+#define N_TURRETS: 3
 
 class Level
 {
+    protected:
+        Player player;
+        
     
 };
 
