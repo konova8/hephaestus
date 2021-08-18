c633c39b9705003b1bba0eab3f733f352b3ff924
OmegaChronos <davide.cioni.128@gmail.com>
2021-08-18 16:55:36 +0200

    Add mapNode structure

--- game.cpp
+++ game.cpp
@@ -5,6 +5,13 @@
 
 using namespace std;
 
+struct mapNode {
+    int index = 0;
+    mapNode *prev = NULL;
+    Level currentLevel; //Assignt to initLevel(int index) 
+    mapNode *next = NULL;
+};
+
 int main() {
-    
+
 }
