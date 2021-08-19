ec3a65d64ca5d008d20f5f5d2e8bfefd296fdbae
OmegaChronos <davide.cioni.128@gmail.com>
2021-08-19 14:21:10 +0200

    Added field and function declarations for level class

--- level.hpp
+++ level.hpp
@@ -1,15 +1,31 @@
 #ifndef LEVEL
 #define LEVEL
 #include "player.hpp"
-#define N_ENEMIES: 4
-#define N_BONUS: 2
-#define N_PLATFORMS: 6 
-#define N_TURRETS: 3
+#include "bonus.hpp"
+#include "enemy.hpp"
+#include "turret.hpp"
+#include "platform.hpp"
+#define N_PLATFORMS 6 
+#define N_ENEMIES 4
+#define N_TURRETS 3
+#define N_BONUSES 2
 
 class Level
 {
     protected:
-        Player player;
+        Platform platforms[N_PLATFORMS];
+        Enemy enemies[N_ENEMIES];
+        Turret turrets[N_TURRETS];
+        Bonus bonuses[N_BONUSES];
+        int updateCounter;
+        int turretUpdateTime;
+        int enemiesUpdateTime;
+    public:
+        Level(int index);
+        void drawLevel();
+        void updateLevel();
+        void exitLevel();
+        
 };
 
 #endif
