be94544b441acb32e8c799f22e85be53dec46f3c
OmegaChronos <davide.cioni.128@gmail.com>
2021-08-19 23:55:05 +0200

    Add backbone of constructor

--- level.cpp
+++ level.cpp
@@ -1,6 +1,32 @@
 #include "level.hpp"
+#include <ncurses.h>
 
 Level::Level(int index)
+{
+    this->width = 40;
+    this->height = 15;
+    setPlatforms(index);
+    setTurrets(index);
+    setEnemies(index);
+    setBonuses(index);
+}
+
+void Level::setPlatforms(int index)
+{
+
+}
+
+void Level::setTurrets(int index)
+{
+    
+}
+
+void Level::setEnemies(int index)
+{
+    
+}
+
+void Level::setBonuses(int index)
 {
     
 }
