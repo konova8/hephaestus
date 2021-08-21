6c5f75179ba26f4f89901e5c2391c4b41ff11e93
OmegaChronos <davide.cioni.128@gmail.com>
2021-08-21 15:36:34 +0200

    Rewrite constructor

--- bullet.cpp
+++ bullet.cpp
@@ -1,20 +1,7 @@
 #include "bullet.hpp"
 
-Bullet::Bullet()
+Bullet::Bullet(int x, int y, int damage, int direction, char symbol) : SingleBlockEntity(x, y, symbol)
 {
-    this->x = 0;
-    this->y = 0;
-    this->damage = 10;
-    this->direction = 1;
-    this->symbol = '*';
-}
-
-
-Bullet::Bullet(int x, int y, int damage, int, char symbol)
-{
-    this->x = x;
-    this->y = y;
     this->damage = damage;
     this->direction = direction;
-    this->symbol = symbol;
 }
