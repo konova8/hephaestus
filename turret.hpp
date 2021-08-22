403b04ef07ee4a49c00f8cfc43697f4fbc8798f9
OmegaChronos <davide.cioni.128@gmail.com>
2021-08-22 11:31:54 +0200

    Fix turret constructor

--- turret.hpp
+++ turret.hpp
@@ -8,7 +8,7 @@ class Turret : public SingleBlockEntity
     protected:
         Bullet bullet;
     public:
-        Turret(bool exists);
+        using SingleBlockEntity::SingleBlockEntity;
         void updateBullet(int xLeft, int xRight); //Moves bullet to appropriate direction (determined based on this->x, turrets with this->x = xLeft are on the left, this->x = xRight are on the right, use SingleBlockEntity::move)
         void resetBullet(); //Brings the bullet back to the turret position
 };
