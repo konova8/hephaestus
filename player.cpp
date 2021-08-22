f3b64ef44bf678a4e547ec118bcb614e5651275b
leo02yt <leo02.yt@gmail.com>
2021-08-22 22:45:05 +0200

    Implement move function

--- player.cpp
+++ player.cpp
@@ -19,3 +19,18 @@ int Player::getPoints()
 {
     return this->points;
 }
+
+void Player::move(int keyPressed){
+    if (keyPressed == 'a' || keyPressed == 'A' || keyPressed == KEY_LEFT){
+        SingleBlockEntity::move(-1, 0);
+    }
+    else if (keyPressed == 's' || keyPressed == 'S' || keyPressed == KEY_DOWN){
+        SingleBlockEntity::move(0, -1);
+    }
+    if (keyPressed == 'W' || keyPressed == 'w' || keyPressed == KEY_UP){
+        SingleBlockEntity::move(0, 1);
+    }
+    if (keyPressed == 'd' || keyPressed == 'D' || keyPressed == KEY_RIGHT){
+        SingleBlockEntity::move(1, 0);
+    }
+}
