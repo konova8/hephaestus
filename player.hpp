e086631500d9f928924b4fb9c4344e10fdb8565e
leo02yt <leo02.yt@gmail.com>
2021-08-23 15:05:45 +0200

    Condensed points and health change functions

--- player.hpp
+++ player.hpp
@@ -13,10 +13,8 @@ class Player : public SingleBlockEntity
         int getXAfterMove(int keyPressed); //Returns the x after the key pressed
         int getYAfterMove(int keyPressed); //Returns the y after the key pressed
         void move(int keyPressed); //Moves based on the key pressed, use SingleBlockEntity::move
-        void removeHealth(int hpsToBeRemoved); //Self explanatory
-        void addHealth(int hpsToBeAdded); //Self explanatory
-        void removePoints(int pointsToBeRemoved); //Self explanatory
-        void addPoints(int pointsToBeAdded); //Self explanatory
+        void healthChange(int change); //Self explanatory
+        void pointsChange(int change); //Self explanatory
         int getHealth(); 
         int getPoints();
 };
