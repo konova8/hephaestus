d261fb6306aa1d0e7f2973b90afad9da30c6fc30
OmegaChronos <davide.cioni.128@gmail.com>
2021-08-24 14:37:09 +0200

    Added declarations of getX/YAfterMove

--- enemy.hpp
+++ enemy.hpp
@@ -9,6 +9,8 @@ class Enemy : public Bullet
         using Bullet::Bullet; //Tells Enemy to use the Bullet::Bullet constructor without making changes to it
         void hitPlayer(Player *player); //Removes this->damage points from player health and suicides (use Bullet::hitPlayer for the former)
         void reverseDirection(); //Reverses the direction (1 = right, -1 = left)
+        int getXAfterMove();
+        int getYAfterMove();
 };
 
 #endif
