e7b0467f6981d73bf33b373622cd781355b05d3d
OmegaChronos <davide.cioni.128@gmail.com>
2021-08-24 16:37:44 +0200

    Fix Bullet::move description

--- bullet.hpp
+++ bullet.hpp
@@ -12,7 +12,7 @@ class Bullet : public SingleBlockEntity
         Bullet(int x = -1, int y = -1, int damage = 10, int direction = 1, char symbol = '*');
         void hitPlayer(Player *player); //Removes this->damage health from Player
         int getDirection();
-        void move();
+        void move(); //moves x of bullet to the this->direction by one single block
 };
 
 #endif
