5e753d264d898c693f32845db7054e9f31712fa7
OmegaChronos <davide.cioni.128@gmail.com>
2021-08-27 15:14:20 +0200

    Change constructor and change bullet pointer

--- turret.cpp
+++ turret.cpp
@@ -1,21 +1,41 @@
 #include "turret.hpp"
 
+Turret::Turret(int x, int y, bool exists, char symbol) : SingleBlockEntity(x, y, exists, symbol)
+{
+    int bulletDirection;
+    if(this->x == 1)
+    {
+        bulletDirection = 1;
+    }
+    else
+    {
+        bulletDirection = -1;
+    }
+    bullet = new Bullet(x, y, bulletDirection, 10, '*');
+}
+
 void Turret::updateBullet()
 {
-    bullet.move();
+    bullet->move();
 }
 
 void Turret::resetBullet()
 {
-    bullet = Bullet(this->x, this->y, bullet.getDirection());
+    int blocksToMove = this->x - bullet->getX();
+    bullet->move(blocksToMove, 0);
 }
 
 int Turret::getBulletDirection()
 {
-    return this->bullet.getDirection();
+    return this->bullet->getDirection();
 }
 
 bool Turret::isBulletColliding(SingleBlockEntity entity)
 {
-    return bullet.isColliding(entity);
+    return bullet->isColliding(entity);
+}
+
+void Turret::printBullet()
+{
+    bullet->print();
 }
--- turret.hpp
+++ turret.hpp
@@ -6,13 +6,14 @@
 class Turret : public SingleBlockEntity
 {
     protected:
-        Bullet bullet;
+        Bullet *bullet;
     public:
-        using SingleBlockEntity::SingleBlockEntity;
+        Turret(int x, int y, bool exists, char symbol);
         void updateBullet(); //Moves the bullet
         void resetBullet(); //Brings the bullet back to the turret position
         int getBulletDirection();
         bool isBulletColliding(SingleBlockEntity entity);
+        void printBullet();
 };
 
 #endif
